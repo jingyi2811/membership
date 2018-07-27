d9486446f5bd73a4f2aa80e1d31cc6753d609e85
mine <dany.batl@mail.com>
2018-07-27 00:55:59 -0700

    login page

--- index.js
+++ index.js
@@ -4,4 +4,4 @@ import { Login } from "./app/pages/login/login";
 import { Merchant2 } from "./app/pages/Merchant2/Merchant2";
 import { Purchase4 } from "./app/pages/Purchase4/Purchase4";
 
-AppRegistry.registerComponent('jimmySignIn', () => Purchase4);
+AppRegistry.registerComponent('jimmySignIn', () => Login);
