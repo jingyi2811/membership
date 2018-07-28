d8c5beced344cc0cec1bd6535cac5b55fd77bd94
Jimmy Lee <jingyi2811@gmail.com>
2018-07-28 14:09:17 +0800

    Change master folder

--- App.js
+++ App.js
@@ -1,6 +1,15 @@
-import { SignUp } from "./app/pages/signup/signup";
-import { Login } from "./app/pages/login/login";
-import { Merchant2 } from "./app/pages/Merchant2/Merchant2";
-
+//import { SignUp } from "./app/pages/signup/signup";
+//import { Login } from "./app/pages/login/login";
+//import { Merchant2 } from "./app/pages/Merchant2/Merchant2";
+//import { App } from './app/components/App2';
 // export default SignUp;
-export default Merchant2;
+//export default Merchant2;
+
+//export default App;
+
+
+//import App from './app/components/App2';
+//export default App;
+
+import AppFinal from './app/components/AppFinal';
+export default AppFinal;
--- index.js
+++ index.js
@@ -1,7 +1,36 @@
 import { AppRegistry } from 'react-native';
-import { SignUp } from "./app/pages/signup/signup";
-import { Login } from "./app/pages/login/login";
-import { Merchant2 } from "./app/pages/Merchant2/Merchant2";
-import { Purchase4 } from "./app/pages/Purchase4/Purchase4";
+//import { SignUp } from "./app/pages/signup/signup";
+//import { Login } from "./app/pages/login/login";
+//import { Merchant2 } from "./app/pages/Merchant2/Merchant2";
+//import { App2 } from './app/components/App2';
+//import { Purchase4 } from "./app/pages/Purchase4/Purchase4";
 
-AppRegistry.registerComponent('jimmySignIn', () => Purchase4);
+
+// Screen 1
+//AppRegistry.registerComponent('jimmySignIn', () => Login);
+
+//Screen 2
+//AppRegistry.registerComponent('jimmySignIn', () => Merchant2);
+
+//Screen 3
+//AppRegistry.registerComponent('jimmySignIn', () => App2);
+
+//Screen 4
+//AppRegistry.registerComponent('jimmySignIn', () => Purchase4);
+
+// Screen 6
+AppRegistry.registerComponent('jimmySignIn', () => App);7
+
+// Screen 7
+design2
+
+// SCreen 8
+//AppRegistry.registerComponent('jimmySignIn', () => SignUp);
+
+import App from './App';
+//AppRegistry.registerComponent('jimmySignIn', () => App2);
+
+
+
+
+
